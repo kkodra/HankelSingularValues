8adc54c65f94c7ccb67d249c0e518bfff6ec0c92
kkodra <ksk2035@icloud.com>
2020-01-14 22:03:43 -0500

    Added function to compute L and H matrices

---
+++ calc_LH.m
@@ -0,0 +1,23 @@
+function [Li,Hi] = calc_LH(A1,A2,A3,A4,eps)
+
+A4inv = inv(A4);
+L0 = A4inv*A3;
+%
+% Newton Method Solution of the L-equation
+%
+for i = 1:10
+  Li = L0;
+  D1i = A4+eps*Li*A2;
+  D2i = -eps*(A1-A2*Li);
+  Qi = A3+eps*Li*A2*Li;
+  Li = lyap(D1i,D2i,-Qi);
+  L0 = Li;
+end
+  errNewton = eps*Li*A1-A4*Li-eps*Li*A2*Li+A3;
+
+% H-equations via the direct method
+Hi = lyap(D2i,D1i,-A2);
+
+% Error test for H
+HiError = Hi*A4-A2+eps*(Hi*Li*A2-A1*Hi+A2*Li*Hi);
+
