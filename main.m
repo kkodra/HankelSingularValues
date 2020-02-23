6eec5de00e533de24c06a1fcdccb0c586bfc03c0
kkodra <ksk2035@icloud.com>
2020-02-22 23:32:19 -0500

    Quick comments

--- main.m
+++ main.m
@@ -58,6 +58,7 @@ C0 = C1-C2*A4inv*A3;
 B0 = B1-A2*A4inv*B2;
 D0 = Dsp-C2*A4inv*B2;
 
+% iter is number of iterations algorithm is evaluated
 [Li,Hi] = calc_LH(A1,A2,A3,A4,eps);
 
 % Slow fast matrices
